c16248ba4b9ad81c628d56e59b36790fc0b00036
Jéssica Carneiro <jessica.carneiro@gympass.com>
2021-09-09 18:22:34 -0300

    Send first message to topic

--- src/main/java/io/github/jessicacarneiro/kafka/tutorial/ProducerDemo.java
+++ src/main/java/io/github/jessicacarneiro/kafka/tutorial/ProducerDemo.java
@@ -2,6 +2,7 @@ package io.github.jessicacarneiro.kafka.tutorial;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
@@ -16,7 +17,10 @@ public class ProducerDemo {
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
         KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+        ProducerRecord<String, String> producerRecord = new ProducerRecord<String, String>("first_topic", "hello_world");
 
-
+        producer.send(producerRecord);
+        producer.flush();
+        producer.close();
     }
 }
