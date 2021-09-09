21ac2c8aa04e862013dcf2aac6403ce31f0e38c7
Jéssica Carneiro <jessica.carneiro@gympass.com>
2021-09-09 18:15:58 -0300

    Add hardcoded settings for Producer

---
+++ src/main/java/io/github/jessicacarneiro/kafka/tutorial/ProducerDemo.java
@@ -0,0 +1,14 @@
+package io.github.jessicacarneiro.kafka.tutorial;
+
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", StringSerializer.class.getName());
+    }
+}
