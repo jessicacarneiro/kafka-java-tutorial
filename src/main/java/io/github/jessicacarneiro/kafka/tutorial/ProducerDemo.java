bfbd44fd8b826b3c5cd2310ae9caa4ebc5189019
Jéssica Carneiro <jessica.carneiro@gympass.com>
2021-09-09 18:17:37 -0300

    Refactor settings for Producer

--- src/main/java/io/github/jessicacarneiro/kafka/tutorial/ProducerDemo.java
+++ src/main/java/io/github/jessicacarneiro/kafka/tutorial/ProducerDemo.java
@@ -1,5 +1,6 @@
 package io.github.jessicacarneiro.kafka.tutorial;
 
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
@@ -7,8 +8,10 @@ import java.util.Properties;
 public class ProducerDemo {
     public static void main(String[] args) {
         Properties properties = new Properties();
-        properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
-        properties.setProperty("key.serializer", StringSerializer.class.getName());
-        properties.setProperty("value.serializer", StringSerializer.class.getName());
+        String bootstrapServers = "127.0.0.1:9092";
+
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
     }
 }
